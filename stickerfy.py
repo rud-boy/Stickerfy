6cfb1451b147be882d4cea683579dd4431d78be2
rud___boy <rudah.tankka@gmail.com>
2021-06-18 15:10:22 -0300

    Added support for gifs.
    Now it is possible to make gif files for animated stickers. No BG
    deleting tool tho.

--- stickerfy.py
+++ stickerfy.py
@@ -9,16 +9,13 @@ def stickerfy(image, stickerLayer, interpolation, borderColor):
     if pdb.gimp_drawable_is_indexed(currentLayer):
         pdb.gimp_image_convert_rgb(image)
 
-    pdb.gimp_image_undo_group_start(image)
     for layer in image.layers:
         pdb.gimp_image_set_active_layer(image, layer)
         pdb.plug_in_autocrop_layer(image, layer)
     pdb.gimp_image_resize_to_layers(image)
     for layer in image.layers:
         pdb.gimp_layer_resize_to_image_size(layer)
-    pdb.gimp_image_undo_group_end(image)
 
-    pdb.gimp_image_undo_group_start(image)
     composedFramesWidth = pdb.gimp_image_width(image)
     composedFramesHeight = pdb.gimp_image_height(image)
     offsetX = (512.0 - composedFramesWidth) / 2
@@ -28,15 +25,12 @@ def stickerfy(image, stickerLayer, interpolation, borderColor):
         scaleFactor = 464.0 / composedFramesWidth
     else:
         scaleFactor = 464.0 / composedFramesHeight
-    pdb.gimp_image_undo_group_end(image)
 
-    pdb.gimp_image_undo_group_start(image)
     for layer in image.layers:
         layerWidth = pdb.gimp_drawable_width(layer) * scaleFactor
         layerHeight = pdb.gimp_drawable_height(layer) * scaleFactor
         pdb.gimp_layer_scale(layer, layerWidth, layerHeight, True)
 
-        frameName = pdb.gimp_item_get_name(layer)
         border = pdb.gimp_layer_new(image, 512, 512, 1, frameName + "B", 100, 0)
         parentPosition = pdb.gimp_image_get_item_position(image, layer) + 1
         pdb.gimp_image_insert_layer(image, border, None, parentPosition)
@@ -46,14 +40,10 @@ def stickerfy(image, stickerLayer, interpolation, borderColor):
         pdb.gimp_selection_none(image)
 
         pdb.script_fu_drop_shadow(image, border, 3, 3, 3, "black", 50.0, 0)
-        #layer = pdb.gimp_image_merge_down(image, layer, 0)
-        #layer = pdb.gimp_image_merge_down(image, layer, 0)
-        #pdb.gimp_item_set_name(layer, frameName)
-    pdb.gimp_image_undo_group_end(image)
 
 register(
     "stickerfy",
-    "Transform a png into a Whatsapp/Telegram sticker.",
+    "Makes pngs/gifs take on a sticker format described by WhatsApp best practices.",
     "Scale to 512x512, add margins, border and dropshadow.",
     "rud___boy",
     "rud___boy",
